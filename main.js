556d60abe3a48c1bd0fce777e6a6442a14e1f05e
Joshua Rivers <joshua.rivers@gmail.com>
2016-06-21 15:48:20 -0400

    added functionality to hide/show divs

---
+++ main.js
@@ -0,0 +1,20 @@
+var processHeader1 = function(target) {
+  $('.accordion-content-one').addClass('show').removeClass('hide')
+  $('.accordion-content-two').removeClass('show').addClass('hide')
+  $('.accordion-content-three').removeClass('show').addClass('hide')
+};
+var processHeader2 = function(target) {
+  $('.accordion-content-one').addClass('hide').removeClass('show')
+  $('.accordion-content-two').addClass('show').removeClass('hide')
+  $('.accordion-content-three').addClass('hide').removeClass('show')
+};
+var processHeader3 = function(target) {
+  $('.accordion-content-one').addClass('hide').removeClass('show')
+  $('.accordion-content-two').addClass('hide').removeClass('show')
+  $('.accordion-content-three').addClass('show').removeClass('hide')
+};
+
+
+$('.accordion-header-one').on('click', processHeader1)
+$('.accordion-header-two').on('click', processHeader2)
+$('.accordion-header-three').on('click', processHeader3)
